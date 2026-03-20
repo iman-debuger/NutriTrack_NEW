31e84bfa921df7bc3fcd84265ed47faa3fca2fdd
mohamedfaisal6767 <mohamedfaisal.676767@gmail.com>
2026-03-20 15:52:48 +0530

    Fix static files configuration for Render

--- nutritrack_project/settings.py
+++ nutritrack_project/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Simplified static files configuration for Render
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
